7845841fa5e52291a97499f8bded1dc808503c32
KronosDP <darrel.danadyaksa19@gmail.com>
2023-10-25 16:53:14 +0700

    rabu 25 okt zoom

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from main.views import show_main
+from main.views import *
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login_user'),
+    path('logout/', logout_user, name='logout'),
+    path('main_login/', show_main_login, name='show_main_login')
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login, logout
+import datetime
 
 # Create your views here.
 def show_main(request):
     context = {}
     return render(request, "main.html", context)
 
+def show_main_login(request):
+    username = request.POST.get('username')
+    context = {"user":username}
+    return render(request, "main_login.html", context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login_user')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if username=="adminulasbuku" and password=="ulasbuku1234":
+                login(request, user)
+                response = HttpResponseRedirect(reverse("main:show_main_login")) 
+                response.set_cookie('last_login', str(datetime.datetime.now()))
+            else:
+                login(request, user)
+                response = HttpResponseRedirect(reverse("main:show_main")) 
+                response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
