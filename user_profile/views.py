f7c301f0a8120603e0d36788bf172a0abf109ca7
Dhemas25 <dhemaswicaksono@gmail.com>
2023-10-28 19:32:30 +0700

    pembaruan user_profile

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,19 +1,34 @@
-from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from user_profile.models import Profile
 from django.contrib.auth.models import User
-from main.views import register_request
+from main.views import login_request, register_request
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def view_profile(request,username):
-    user = get_object_or_404(User)  # Ambil berdasarkan username yang diberikan
-    username = user.username
+    user = get_object_or_404(User,username=username)  # Ambil berdasarkan username yang diberikan
+
     context = {
-        'user': username, 
+        'user': user, 
     }
 
     return render(request, 'profile_page.html', context)
 
-def update_user(request):
-    return render(request, "update_user.html", {} )
+def add_description(request, username):
+    if request.method == 'POST':
+        description = request.POST.get('description', '')
+
+        # Simpan deskripsi ke profil pengguna
+        user = get_object_or_404(User, username=username)
+        user.profile.description = description
+        user.profile.save()
+
+        # Anda juga dapat memvalidasi data deskripsi di sini jika diperlukan
+
+        return JsonResponse({'success': True})
+    else:
+        return JsonResponse({'success': False, 'error': 'Invalid request method'})
+
     
