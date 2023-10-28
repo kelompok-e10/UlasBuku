f5e86b57ace6cbbb0130da0fdae87c5d8ccd54fe
Dhemas25 <dhemaswicaksono@gmail.com>
2023-10-29 03:42:32 +0700

    Mengubah tampilan html profile

--- user_profile/models.py
+++ user_profile/models.py
@@ -6,7 +6,11 @@ from django.db.models.signals import post_save
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
-    
+    first_name = models.TextField(blank=True, null=True)
+    last_name = models.TextField(blank=True, null=True)
+    email = models.TextField(blank=True, null=True)
+    contact = models.TextField(blank=True, null=True)
+
 def create_profile(sender,instance,created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
--- user_profile/views.py
+++ user_profile/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from main.views import login_request, register_request
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def view_profile(request, username):
     user = get_object_or_404(User, username=username)
 
@@ -14,6 +15,7 @@ def view_profile(request, username):
 
     return render(request, 'profile_page.html', context)
 
+@login_required
 def add_description(request, username):
     if request.method == 'POST':
         description = request.POST.get('description', '')
@@ -33,6 +35,47 @@ def get_description(request, username):
     user = get_object_or_404(User, username=username)
     description = user.profile.description if user.profile.description else ""
     return JsonResponse({'description': description})
+
+@login_required
+def edit_profile(request, username):
+    user = get_object_or_404(User, username=username)
+
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name', '')
+        last_name = request.POST.get('last_name', '')
+        email = request.POST.get('email', '')
+        contact = request.POST.get('contact', '')
+
+        # Simpan data profil yang diperbarui
+        user.profile.first_name = first_name
+        user.profile.last_name = last_name
+        user.profile.email = email
+        user.profile.contact = contact
+        user.save()
+
+        return JsonResponse({'success': True})
+    else:
+        return JsonResponse({'success': False, 'error': 'Invalid request method'})
+
+def get_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    user_profile = user.profile  # Mendapatkan objek Profile yang sesuai dengan pengguna
+
+    # Sekarang, Anda bisa mengakses atribut-atribut profil seperti first_name, last_name, email, contact_phone, dll.
+    first_name = user_profile.first_name
+    last_name = user_profile.last_name
+    email = user_profile.email
+    contact = user.profile.contact
+    
+    # Kemudian Anda dapat mengembalikan data profil dalam format JSON atau sejenisnya.
+    profile_data = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
+        'contact': contact,
+    }
+
+    return JsonResponse(profile_data)
 
 
     
