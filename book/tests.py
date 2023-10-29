ba97b1c1589d7a89ec3d844b5cad23eaa78a39f8
KronosDP <darrel.danadyaksa19@gmail.com>
2023-10-29 22:54:50 +0700

    edit add book, add unit test for book app

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class BookTest(TestCase):
+    def test_api_books_returns_json(self):
+        response = self.client.get('/api/books/')
+        self.assertEqual(response['Content-Type'], 'application/json')
