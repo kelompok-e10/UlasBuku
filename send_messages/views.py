63db4f95e7a76678162c452c0c79168f2d353a94
suryaraavi <surya.raavi@ui.ac.id>
2023-12-16 19:32:10 +0700

    Update send_messages dan menambahkan authentication logout

--- send_messages/views.py
+++ send_messages/views.py
@@ -7,9 +7,12 @@ from django.urls import reverse
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
-from django.db.models import Q
+from django.db.models import Q, Max
 from django.template import loader
 from main import*
+import json
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url="main:login")
 def show_messages(request):
@@ -51,3 +54,76 @@ def user_messages_by_id(request, selected_user_id):
 @login_required(login_url="main:login")
 def send(request, recipient_id):
     sender_id = request.user.id
+
+def get_json(request):
+    data = Messages.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+@csrf_exempt
+def user_info_for_flutter(request, username):
+    if request.method == 'GET':
+        # Temukan pengguna saat ini
+        current_user = User.objects.get(username=username)
+
+        # Temukan semua pengguna lain yang berkomunikasi dengan pengguna saat ini
+        other_users = User.objects.exclude(id = current_user.id).order_by('username')
+
+        # Inisialisasi dictionary untuk menyimpan last message dari setiap user
+        data = {}
+
+        # Loop melalui setiap pengguna lain
+        for other_user in other_users:
+            # Ambil pesan terakhir antara pengguna saat ini dan pengguna lain
+            last_message = Messages.objects.filter(
+                Q(sender=current_user, recipient=other_user) | Q(
+                    sender=other_user, recipient=current_user)
+            ).order_by('-timestamp').first()
+            # other_username = User.objects.get(id=other_user).username
+            if last_message is not None:
+                data[other_user.username] = {
+                    'fields': {
+                        'user id': current_user.id,
+                        'selected user': other_user.username,
+                        'selected user id': other_user.id,
+                        'text': last_message.text
+                    }
+                }
+            else:
+                field = {
+                    'fields': {
+                        'user id': current_user.id,
+                        'selected user': other_user.username,
+                        'selected user id': other_user.id,
+                        'text': 'Mulai percakapan'
+                    }
+                }
+                data[other_user.username] = field
+        return HttpResponse(json.dumps(data), content_type="application/json")
+
+@csrf_exempt
+def get_messages_flutter(request, current_user, selected_user):
+    if request.method == 'GET':
+        current_user_id = User.objects.get(username=current_user)
+        selected_user_id = User.objects.get(username=selected_user)
+        messages = Messages.objects.filter(Q(sender=current_user_id, recipient=selected_user_id) | Q(
+                   sender=selected_user_id, recipient=current_user_id)).order_by("-timestamp")
+        
+        return HttpResponse(serializers.serialize("json", messages), content_type="application/json")
+
+@csrf_exempt
+def send_messages_flutter(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        recipient = User.objects.get(id=int(data.get('recipient')))
+        new_messages = Messages.objects.create(
+            sender = request.user,
+            recipient = recipient,
+            text = data.get('text'),
+        )  
+        new_messages.save()
+        return JsonResponse({'status': 'success'}, status = 200)
+    else:
+        return JsonResponse({'status': 'error'}, status = 401)
+
+
+ 
