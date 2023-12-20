c7d1734c6e7d9f7d6ba8f26785438a2019db8536
suryaraavi <surya.raavi@ui.ac.id>
2023-12-20 22:00:57 +0700

    Update register 2

--- UlasBuku/settings.py
+++ UlasBuku/settings.py
@@ -77,7 +77,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
-CSRF_TRUSTED_ORIGINS = ["https://ulasbuku-e10-tk.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["https://ulasbuku-e10-tk.pbp.cs.ui.ac.id", 'http://127.0.0.1:8000']
 
 ROOT_URLCONF = 'UlasBuku.urls'
 
--- authentication/views.py
+++ authentication/views.py
@@ -5,6 +5,9 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 import re
+from django.db import IntegrityError
+from django.contrib.auth.backends import UserModel
+
 
 @csrf_exempt
 def login(request):
@@ -35,51 +38,43 @@ def login(request):
 
 @csrf_exempt
 def register(request):
-    # print("ASUU")
     data = json.loads(request.body)
-    print(data);
     username = data.get('username')
-    password = data.get('password')
+    password1 = data.get('password')
     password_confirmation = data.get('password_confirmation')
- 
-    # # Password validation
-    # if len(password) < 8:
-    #     return JsonResponse({
-    #         "status": False,
-    #         "message": "Registrasi gagal, password harus paling sedikit 8 karakter."
-    #     }, status=400)
-    # elif not re.search("[A-Z]", password):
-    #     return JsonResponse({
-    #         "status": False,
-    #         "message": "Registrasi gagal, password harus memiliki setidaknya satu huruf kapital."
-    #     }, status=400)
-    # elif not re.search("[0-9]", password):
-    #     return JsonResponse({
-    #         "status": False,
-    #         "message": "Registrasi gagal, password harus memiliki setidaknya satu angka."
-    #     }, status=400)
-    # elif password != password_confirmation:
-    #     return JsonResponse({
-    #         "status": False,
-    #         "message": "Registrasi gagal, password dan password tidak sesuai."
-    #     }, status=400)
+    print(username)
+    print(data)
+
+    if UserModel.objects.filter(username=username).exists():
+        return JsonResponse({"status": "duplicate"}, status=401)
+
+    if password1 != password_confirmation:
+        return JsonResponse({"status": "pass failed"}, status=401)
     
-    # Username validation
-    # if User.objects.filter(username=username).exists():
+    createUser = User.objects.create_user(
+        username = username, 
+        password = password1,
+    )
+    print(createUser)
+    createUser.save()
+    return JsonResponse({
+            "username": username,
+            "status": "success",
+            "message": f"Registrasi berhasil! User dengan nama {username} berhasil dibuat."
+    }, status=200)
+    # try:
+    #     user = User.objects.create_user(username=username, email=username, password=password)
+    #     user.save()
+    #     return JsonResponse({
+    #         "username": user.username,
+    #         "status": True,
+    #         "message": f"Registrasi berhasil! User dengan nama {username} berhasil dibuat."
+    #     }, status=200)
+    # except IntegrityError:
     #     return JsonResponse({
     #         "status": False,
     #         "message": "Registrasi gagal, username telah digunakan."
     #     }, status=400)
-        
-    # Create user
-    if (User.objects.create_user(username=username, email=username, password=password) != None):
-        user = User.objects.create_user(username=username, email=username, password=password)
-        user.save()
-        return JsonResponse({
-            "username": user.username,
-            "status": True,
-            "message": "Registrasi berhasil! User dengan nama {username} berhasil dibuat."
-        }, status=200)
         
 @csrf_exempt
 def logout(request):
